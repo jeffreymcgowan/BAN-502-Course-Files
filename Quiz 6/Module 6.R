5b424b3aca69a973a445df7c84e0c4a959b19547
Jeffrey McGowan <jeffrey.mcgowan@gmail.com>
2023-10-04 15:10:48 -0400

    Add files via upload

---
+++ Quiz 6/Module 6.R
@@ -0,0 +1,17 @@
+#Quiz 6
+
+rm(list=ls()) #clean env
+
+# Helper packages
+library(tidyverse)
+library(tidymodels)
+library(cluster)
+library(factoextra)
+library(dendextend)
+
+#####################################################
+
+trucks <- read_csv("trucks-1.csv")
+
+#####################################################
+
